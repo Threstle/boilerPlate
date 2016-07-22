c5f2b58698933e909507c662d4fe646032f3f9ea
Threstle <Etienne.chaumont@gmail.com>
2016-07-22 17:20:18 +0200

    update boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # boilerplate
+
+## Installation
+
+### npm install
+### bower install
+
+Si le bower_install ne fonctionne pas, autoriser écriture/lecture dans tout les sous dossiers du projet.
--- js/util.js
+++ js/util.js
@@ -6,3 +6,74 @@ function scale(x,min,max,nuMin,nuMax){
     return nuX;
     
 }
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i -= 1) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+
+function isMobile(){
+    var ismobi = (navigator.userAgent.match(/Mobi/i));
+    
+    if(ismobi && navigator.userAgent.match(/iPad/i)){
+      ismobi = false;
+    }
+
+    if(ismobi){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+function isIpad(){
+    var ipad = (navigator.userAgent.match(/iPad/i));
+
+    if(ipad){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+function isIE() { 
+  return ((navigator.appName == 'Microsoft Internet Explorer') || ((navigator.appName == 'Netscape') && (new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})").exec(navigator.userAgent) != null))); 
+}
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function cleanArray(ar,deleteValue) {
+  for (var i = 0; i < ar.length; i++) {
+    if (ar[i] == deleteValue) {         
+      ar.splice(i, 1);
+      i--;
+    }
+  }
+  return ar;
+};
+
+function isElementInViewport (el,scroll) {
+
+    if(scroll > el.offset().top && scroll < el.offset().top + $(window).height()){
+      return true;
+    }
+    else{
+      return false;
+    }
+}
